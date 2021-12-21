cfd10b378eb0645c2cab34a5f0c6986aba142d60
unknown <giovane1-8>
2021-12-21 17:21:40 -0300

    conexao com o banco

--- Models/Model.php
+++ Models/Model.php
@@ -3,6 +3,7 @@
  * Classe filha de todas as Models
  * aqui fica os metodos e propriedades que usamos em todas as Model
  * aqui fica a conexão com o servidor de banco de dados
+ * MODIFICAR VARIAVEIS: $username, $password, $servidor PARA CONEXAO COM O SERVIODR 
 */
 
 namespace Models;
@@ -14,15 +15,16 @@ class Model
     protected object $PDO;
     function __construct()
     {
-        /*
+        
         $username = "root";
         $password = "";
+        $servidor = "localhost:3306";
         try {
-            $this->PDO = new \PDO('mysql:host=localhost:3306;dbname=db_mangakindle', $username, $password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->PDO = new \PDO('mysql:host='.$servidor.';dbname=db_blog', $username, $password, array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             $this->PDO->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             echo 'ERROR: ' . $e->getMessage();
         }
-        */
+        
     }
 }
