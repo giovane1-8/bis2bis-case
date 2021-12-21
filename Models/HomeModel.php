9b180ef7a034442f19702461f78a0ba7703516e2
unknown <giovane1-8>
2021-12-21 17:34:32 -0300

    validando login

--- Models/HomeModel.php
+++ Models/HomeModel.php
@@ -5,5 +5,30 @@
     namespace Models;
 
     class HomeModel extends Model{
-        
+        function getResultado(): bool{
+            return $this->resultado;
+        }
+
+        public function validarLogin(array $dados = null){
+            
+            $query = "SELECT * FROM tb_usuario WHERE nm_email = :email AND nm_senha = :senha LIMIT 1";
+            $result = $this -> PDO ->prepare($query);
+            $result -> bindParam(':email', $dados['usuario'], \PDO::PARAM_STR);
+            $result -> bindParam(':senha', $dados['senha'], \PDO::PARAM_STR);
+            $result -> execute();
+            $return = $result -> fetch();
+            
+            if(!empty($return)){
+                $_SESSION['isLogado'] = true;
+                $_SESSION['id_usuario'] = $return['id_usuario'];
+                $_SESSION['nm_usuario'] = $return['nm_usuario'];
+                $_SESSION['nm_senha'] = $return['nm_senha'];
+                $_SESSION['nm_email'] = $return['nm_email'];
+                $_SESSION['nm_vip'] = $return['nm_vip'];
+                $this -> resultado = true;
+            }else{ 
+                $_SESSION['isLogado'] = false;
+                $this -> resultado = false;
+            }
+        }
     }
--- Views/paginas/login.php
+++ Views/paginas/login.php
@@ -27,14 +27,14 @@
         <form class="col" method="post" action="#">
 
             <div class="form-group">
-                <label for="exampleInputEmail1">Endereço de email ou Nickname</label>
+                <label for="exampleInputEmail1">Endereço de email:</label>
                 <input type="text" class="form-control" name="usuario" aria-describedby="emailHelp" placeholder="Seu email ou nickname" value="<?php if (isset($_SESSION["usuario"])) {
                                                                                                                                                     echo $_SESSION["usuario"];
                                                                                                                                                 } ?>" required>
                 <small id="emailHelp" class="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Senha</label>
+                <label for="exampleInputPassword1">Senha:</label>
                 <input type="password" name="senha" class="form-control" placeholder="Senha" required>
             </div>
 
