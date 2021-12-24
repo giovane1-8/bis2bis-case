1cb6e7aafbbbd0f1ef3e0271e5215574967aaca1
unknown <giovane1-8>
2021-12-24 06:14:16 -0300

    Adicionando ON DELETE CASCADE no dump do banco

--- db_blog.sql
+++ db_blog.sql
@@ -46,7 +46,8 @@ CREATE TABLE IF NOT EXISTS `tb_post` (
   `id_usuario` int(11) NOT NULL,
   `dt_post` date NOT NULL,
   PRIMARY KEY (`id_post`),
-  KEY `fk_tb_post_tb_usuario_idx` (`id_usuario`) ON DELETE CASCADE
+  KEY `fk_tb_post_tb_usuario_idx` (`id_usuario`),
+  CONSTRAINT `fk_tb_post_tb_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario` (`id_usuario`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
 
 
